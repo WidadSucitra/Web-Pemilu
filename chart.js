a9182280abdf00f5f8eab2c4933c887737472aa4
Andi Widad Sucitra <sucitraandiwidad@gmail.com>
2020-07-06 12:04:27 +0800

    Modified chart.js and update.js

--- chart.js
+++ chart.js
@@ -6,7 +6,7 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ["00.00", "00.03", "00.06", "00.09", "00.12", "00.15", "00.18", "00.21", "00.24"],
         datasets: [{
             label: false ,
             backgroundColor: 'transparent',
@@ -31,7 +31,7 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ["00.00", "00.03", "00.06", "00.09", "00.12", "00.15", "00.18", "00.21", "00.24"],
         datasets: [{
             label: 'My First dataset',
             backgroundColor: 'transparent',
@@ -49,51 +49,5 @@ var chart = new Chart(ctx, {
         tooltips: {
             mode: 'nearest'
         }
-    }
-});
-
-// CHART UPDATE PEROLEHAN SUARA KETUA HMIF
-var ctx = document.getElementById('update1').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: false ,
-            backgroundColor: 'transparent',
-            borderColor: 'black',
-            data: [4, 7, 11, 15, 20, 21, 24]
-        },{
-            label: 'My Second dataset',
-            backgroundColor: 'transparent',
-            borderColor: '#6397BD',
-            data: [3, 6, 9, 12, 15, 19, 21]
-        }]
-    }
-});
-
-// CHART UPDATE PEROLEHAN SUARA KETUA DMIF
-var ctx = document.getElementById('chart2-update').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: false ,
-            backgroundColor: 'transparent',
-            borderColor: 'black',
-            data: [4, 7, 11, 15, 20, 21, 24]
-        },{
-            label: 'My Second dataset',
-            backgroundColor: 'transparent',
-            borderColor: '#6397BD',
-            data: [3, 6, 9, 12, 15, 19, 21]
-        }]
     }
 });
