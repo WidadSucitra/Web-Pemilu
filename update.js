f489e6d5e999977a850a9deb6ba9fe4bd70de9f3
WidadSucitra <62865740+WidadSucitra@users.noreply.github.com>
2020-07-08 16:36:41 +0800

    Update update.js

--- update.js
+++ update.js
@@ -1,5 +1,7 @@
+let xlabel = [0];
+let interval = 3000 * 60 * 60 ;
 
-// CHART UPDATE PEROLEHAN SUARA KETUA HMIF
+
 var ctx = document.getElementById('update1').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -7,22 +9,38 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ["00.00", "00.03", "00.06", "00.09", "00.12", "00.15", "00.18", "00.21", "00.24"],
+        labels: xlabel,
         datasets: [{
-            label: false ,
             backgroundColor: 'transparent',
             borderColor: 'black',
             data: [4, 7, 11, 15, 20, 21, 24]
         },{
-            label: 'My Second dataset',
             backgroundColor: 'transparent',
             borderColor: '#6397BD',
             data: [3, 6, 9, 12, 15, 19, 21]
         }]
+    },
+    options: {
+        legend: {
+        display: false,
+        }
     }
 });
 
-// CHART UPDATE PEROLEHAN SUARA KETUA DMIF
+chart.render();
+setInterval(function() {waktu()}, 1000);
+
+function waktu() {
+let d = new Date();
+let n = d.getHours();
+xlabel.push(n)
+document.getElementById('tes1').innerHTML = xlabel;
+chart.render();
+};
+
+
+
+// CHART PEROLEHAN SUARA KETUA DMIF
 var ctx = document.getElementById('update2').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -30,17 +48,20 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ["00.00", "00.03", "00.06", "00.09", "00.12", "00.15", "00.18", "00.21", "00.24"],
+        labels: xlabel,
         datasets: [{
-            label: false ,
             backgroundColor: 'transparent',
             borderColor: 'black',
             data: [4, 7, 11, 15, 20, 21, 24]
         },{
-            label: 'My Second dataset',
             backgroundColor: 'transparent',
             borderColor: '#6397BD',
             data: [3, 6, 9, 12, 15, 19, 21]
         }]
+    },
+    options: {
+        legend: {
+        display: false,
+        }
     }
 });
