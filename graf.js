f2c97e242e800788c2caea0f408f24b6c3ca8f45
arusmiati <62420869+arusmiati@users.noreply.github.com>
2020-07-25 21:24:51 +0800

    Update graf.js

--- graf.js
+++ graf.js
@@ -1,91 +1,115 @@
-var data_ketude1 = "<?= $ketude1; ?>";
-var data_ketude2 = "<?= $ketude2; ?>";
-var data_ketum1 = "<?= $ketum1;  ?>";
-var data_ketum2 = "<?= $ketum2;  ?>";
+$(document).ready(function() {
+    let xlabel = [0];
+    let interval = 3000 * 60 * 60 ;
 
-var chart = new Highcharts.Chart({
-    chart: {
-        renderTo: 'graf',
-        type: 'column',
-        options3d: {
-            enabled: true,
-            alpha: 0,
-            beta: 25,
-            depth: 50,
-            viewDistance: 50
-        }
-    },
-    xAxis: {
-        categories: ['Calon Pengurus']
-    },
-    yAxis: {
-        title: {
-            text: 'Total Suara'
-        }
+    var ctx = document.getElementById('chart1').getContext('2d');
+    var data = {
+        labels: xlabel,
+        datasets: [{
+            label: "Ketude 1",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [0, 50, 100, 90, 30]
+            
+            }
+            , {
+            label: "Ketude 2",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [0, 100, 80, 55, 10] 
+            
+        }]
+    };
 
-    },
-    title: {
-        text: 'Grafik Perhitungan Suara'
-    },
-    subtitle: {
-        text: ''
-    },
-    plotOptions: {
-        column: {
-            depth: 150
-        }
-    },
-    series: [{
+    var options = {}
 
-        name: 'Ketum 1',
-        data: [70]
-    }, {
-        name: 'Ketum 2',
-        data: [18]
-    }]
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: options
+    });
+    
+    setInterval(function() {
+        waktu();
+        
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: data,
+            options: options
+        });
+    }, 5000);
 
+    function waktu() {
+        let d = new Date();
+        let n = d.getHours();
+        xlabel.push(n)
+        // document.getElementById('tes1').innerHTML = xlabel;
+        chart.render();
+    };
 });
 
+$(document).ready(function() {
+    let xlabel = [0];
+    let interval = 3000 * 60 * 60 ;
 
-var chart = new Highcharts.Chart({
-    chart: {
-        renderTo: 'container',
-        type: 'column',
-        options3d: {
-            enabled: true,
-            alpha: 0,
-            beta: 25,
-            depth: 50,
-            viewDistance: 50
-        }
-    },
-    xAxis: {
-        categories: ['Calon Pengurus']
-    },
-    yAxis: {
-        title: {
-            text: 'Total Suara'
-        }
+    var ctx = document.getElementById('chart2').getContext('2d');
+    var data = {
+        labels: xlabel,
+        datasets: [{
+            label: "Ketum 1",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [0, 100, 80, 55, 10]
+            
+            }
+            , {
+            label: "Ketum 2",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [0, 70, 40, 105, 10] 
+            
+        }]
+    };
 
-    },
-    title: {
-        text: 'Grafik Perhitungan Suara'
-    },
-    subtitle: {
-        text: ''
-    },
-    plotOptions: {
-        column: {
-            depth: 150
-        }
-    },
-    series: [{
-        name: 'Ketude 1',
-        data: [50]
-    }, {
-        name: 'Ketude 2',
-        data: [10]
+    var options = {}
 
-    }]
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: options
+    });
+    
+    setInterval(function() {
+        waktu();
+        
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: data,
+            options: options
+        });
+    }, 5000);
 
+    function waktu() {
+        let d = new Date();
+        let n = d.getHours();
+        xlabel.push(n)
+        // document.getElementById('tes1').innerHTML = xlabel;
+        chart.render();
+    };
 });
